1f8969c176c10d60c170a494675e21f52dece059
Piyush Dharmani <37998577+PiyushDharmani@users.noreply.github.com>
2018-12-30 14:47:29 +0530

    Add files via upload

--- ui.R
+++ ui.R
@@ -2,14 +2,19 @@
 library(shiny)
 
 shinyUI(fluidPage(
-  titlePanel("Histogram if selected Sample Size"),
+  titlePanel("Histogram for selected Sample Size"),
   sidebarLayout(
     sidebarPanel(
        numericInput(inputId = "n",
-                    "Sample Size",value = 30)
+                    "Sample Size",value = 30),
+       helpText("Select the sample size")
     ),
     mainPanel(
-       plotOutput(outputId="distPlot")
-    )
+       plotOutput(outputId="distPlot"),
+       h3("Instructions"),
+       h4("1.Select the input sample size"),
+       h4("2.The graph will be generated automatically")
+       
+       )
   )
 ))
